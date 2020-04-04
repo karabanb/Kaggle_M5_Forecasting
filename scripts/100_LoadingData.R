1127a7c41cf6564e447a6fff885f3a22f1573951
karabanb <bkarabands@gmail.com>
2020-04-04 20:54:10 +0200

    raw data frames has been created

---
+++ scripts/100_LoadingData.R
@@ -0,0 +1,10 @@
+
+### CONVERTING CSVs to DATA.FRAMEs #####################################################################################
+
+calendar <- read.csv(file = 'data/raw_data/calendar.csv')
+sell_prices <- read.csv(file = 'data/raw_data/sell_prices.csv')
+sales_train_validation <- read.csv(file = 'data/raw_data/sales_train_validation.csv')
+sample_submission <- read.csv(file = 'data/raw_data/sample_submission.csv')
+
+save(calendar, sell_prices, sales_train_validation, sample_submission, file = 'data/100_raw_data.RData')
+
