a10181f9e8e800db821036571edccb084e75c741
karabanb <bkarabands@gmail.com>
2020-04-21 08:01:20 +0200

    file's and object's names has been chanched

--- scripts/001_LoadingRawData.R
+++ scripts/001_LoadingRawData.R
@@ -6,5 +6,5 @@ sell_prices <- read.csv(file = 'data/raw_data/sell_prices.csv')
 sales_train_validation <- read.csv(file = 'data/raw_data/sales_train_validation.csv')
 sample_submission <- read.csv(file = 'data/raw_data/sample_submission.csv')
 
-save(calendar, sell_prices, sales_train_validation, sample_submission, file = 'data/100_raw_data.RData')
+save(calendar, sell_prices, sales_train_validation, sample_submission, file = 'data/001_raw_data.RData')
 
